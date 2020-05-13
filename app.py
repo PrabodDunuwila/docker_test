76f837c8a8ff96f5cbccd354090ddc9fa0a5ecff
Prabod Dunuwila <praboddunuwila@gmail.com>
2020-05-13 15:33:19 +0530

    Simple hello world

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-import os
-
 from flask import Flask
 
 app = Flask(__name__)
 
-name = os.environment.get('YOUR_NAME')
-
 
 @app.route("/")
-@app.route("/hello")
-def main():
-    print(name)
+def hello():
     return '''
         <html>
             <head>
                 <title>Home Page</title>
             </head>
             <body>
-                <h1>Hello, ''' + name + '''!</h1>
+                <h1>Hello, world!</h1>
             </body>
         </html>'''
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="8082")
+    app.run(debug=True, host="0.0.0.0")
